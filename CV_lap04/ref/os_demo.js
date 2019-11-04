cc80c76cce1ec223ca8f333885979ecf05f8062c
fsuriya <flukbanrong@gmail.com>
2019-11-04 23:53:20 +0700

    first my website

--- CV_lap04/ref/os_demo.js
+++
@@ -1,8 +0,0 @@
-const os = require('os');
-
-console.log(os.platform());
-console.log(os.arch());
-// console.log(os.cpus());
-console.log(os.freemem()+'/'+os.totalmem());
-console.log(os.homedir());
-console.log(os.uptime()+'s');
