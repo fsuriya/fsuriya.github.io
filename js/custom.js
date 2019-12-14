df6332b6febf3c06679bc233d0b7b46daf4daa6f
fsuriya <flukbanrong@gmail.com>
2019-12-14 21:51:00 +0700

    pop up

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,9 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+
+    $("#demo02").animatedModal02();
 
     // Contact Form 	
 
